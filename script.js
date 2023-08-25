a6f020c5b666499cc263267669510ad684921d07
MvpYJ <134731948+MvpYJ@users.noreply.github.com>
2023-08-25 23:15:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+ 
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const chatInput = document.getElementById("chat-input");
+    const chatMessages = document.getElementById("chat-messages");
+
+    function sendMessage() {
+        const message = chatInput.value.trim();
+        if (message !== "") {
+            chatMessages.innerHTML += `<div class="message user">${message}</div>`;
+            chatInput.value = "";
+
+            // Send message via email (you'll need to implement this)
+            sendEmail(message);
+        }
+    }
+
+    document.getElementById("send-button").addEventListener("click", sendMessage);
+    chatInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            sendMessage();
+        }
+    });
+});
